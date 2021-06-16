ad21d36135265a29814964c6bfad0e404d1b2669
Joseph Weinkam <jweinkam@filmrobot.com>
2021-06-16 13:34:23 -0700

    setup basic api project for a docx to pdf covert service using aspose

---
+++ word-to-pdf/Controllers/DocumentController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Aspose.Words;
+using Microsoft.AspNetCore.Mvc;
+using word_to_pdf.Models;
+
+namespace word_to_pdf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+
+        [HttpPost("convert")]
+        public async Task<IActionResult> ConvertToPdf([FromBody] DocumentData doc)
+        {
+            try
+            {
+                byte[] byteArray = Convert.FromBase64String(doc.body);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(byteArray, 0, byteArray.Length);
+
+                    Document convertedDoc = new Document(memoryStream);
+
+                    using (MemoryStream dstStream = new MemoryStream())
+                    {
+                        convertedDoc.Save(dstStream, SaveFormat.Pdf);
+                        dstStream.Position = 0;
+                        byte[] pdfBytes = dstStream.ToArray();
+
+                        var res = System.Convert.ToBase64String(pdfBytes);
+                        return await Task.FromResult(Ok(res));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
---
+++ word-to-pdf/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace word_to_pdf
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            string licenseString = Configuration["ASPOSE_LICENSE"];
+            byte[] licenseBytes = Convert.FromBase64String(licenseString);
+            Aspose.Words.License license = new Aspose.Words.License();
+            try
+            {
+                // Initializes a license from a stream 
+                MemoryStream stream = new MemoryStream(licenseBytes);
+                license.SetLicense(stream);
+                Console.WriteLine("License set successfully.");
+            }
+            catch (Exception e)
+            {
+                // We do not ship any license with this example, visit the Aspose site to obtain either a temporary or permanent license. 
+                Console.WriteLine("\nThere was an error setting the license: " + e.Message);
+            }
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
