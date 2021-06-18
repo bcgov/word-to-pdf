f904894875e0f594c0779bb8b6d1bcd94daa7448
Joseph Weinkam <jweinkam@filmrobot.com>
2021-06-18 11:32:05 -0700

    set license

--- word-to-pdf/Startup.cs
+++ word-to-pdf/Startup.cs
@@ -13,21 +13,21 @@ namespace word_to_pdf
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            // string licenseString = Configuration["ASPOSE_LICENSE"];
-            // byte[] licenseBytes = Convert.FromBase64String(licenseString);
-            // Aspose.Words.License license = new Aspose.Words.License();
-            // try
-            // {
-            //     // Initializes a license from a stream 
-            //     MemoryStream stream = new MemoryStream(licenseBytes);
-            //     license.SetLicense(stream);
-            //     Console.WriteLine("License set successfully.");
-            // }
-            // catch (Exception e)
-            // {
-            //     // We do not ship any license with this example, visit the Aspose site to obtain either a temporary or permanent license. 
-            //     Console.WriteLine("\nThere was an error setting the license: " + e.Message);
-            // }
+            string licenseString = Configuration["ASPOSE_LICENSE"];
+            byte[] licenseBytes = Convert.FromBase64String(licenseString);
+            Aspose.Words.License license = new Aspose.Words.License();
+            try
+            {
+                // Initializes a license from a stream 
+                MemoryStream stream = new MemoryStream(licenseBytes);
+                license.SetLicense(stream);
+                Console.WriteLine("License set successfully.");
+            }
+            catch (Exception e)
+            {
+                // We do not ship any license with this example, visit the Aspose site to obtain either a temporary or permanent license. 
+                Console.WriteLine("\nThere was an error setting the license: " + e.Message);
+            }
         }
 
         public IConfiguration Configuration { get; }
